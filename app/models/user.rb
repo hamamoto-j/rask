7fc180b60ca7e3259b8bb1f72076d4793936377f
hamamoto-j <p5eq792y@s.okayama-u.ac.jp>
2021-06-09 17:33:22 +0900

    Fix signup error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :todo, :foreign_key => :todo_id
+  belongs_to :todo, optional: true
   validates :name, presence: true, length: { maximum: 50 }
 
   has_secure_password
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-             BCrypt::Engine.cost
+      BCrypt::Engine.cost
     BCrypt::Password::create(string, cost: cost)
   end
 end
