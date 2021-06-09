9b4a045f8018c45b0379d51423cf234593e3663b
hamamoto-j <p5eq792y@s.okayama-u.ac.jp>
2021-06-09 23:22:32 +0900

    Update view html

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/home', to: 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/signup', to: 'users#new'
+  root "static_pages#home"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/home", to: "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/signup", to: "users#new"
+  get "/projects", to: "projects#index"
+  get "/todos", to: "todos#index"
 
   resources :todos
   resources :projects
