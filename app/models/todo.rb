f1fe7aa998d45c3fbadd66fcd6d857e6d58f127a
hamamoto-j <p5eq792y@s.okayama-u.ac.jp>
2021-06-09 17:08:25 +0900

    Addproject in todo form

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,4 @@
 class Todo < ApplicationRecord
+  belongs_to :project, :foreign_key => :project_id
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  belongs_to :todo, :foreign_key => :todo_id
 end
